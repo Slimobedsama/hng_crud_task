d36df3ada9df57819ab360e44dbb9b82e6862c01
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-11 13:56:53 +0100

    added middlewares for json parser, url parser and user

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const db = require('./config/db');
+const userRouter = require('./router/userRoute');
 const dotenv = require('dotenv');
 dotenv.config();
 db();
-
 const PORT = process.env.PORT;
+// middleware to parse json data
+app.use(express.json());
+// middleware to parse url encoded data
+app.use(express.urlencoded({extended: true}));
+// user middleware
+app.use('/api', userRouter);
 
 
 app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
