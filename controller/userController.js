c5bacaaaa9d44ac0bbb7f543d0d027cf0ae22f68
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-11 18:09:06 +0100

    installation of npm validator package and implementation of validation on the create function

--- controller/userController.js
+++ controller/userController.js
@@ -1,13 +1,19 @@
 const User = require('../model/userModel');
+const validator = require('validator');
 
-exports.create = async(req, res)=> {
+exports.create = async(req, res, next)=> {
+    const {name} = req.body;
     try {
-        const newUser = await User.create();
-        if(!newUser) {
-            throw new Error('Bad Request');
+        if(validator.isEmpty(name)) {
+            throw new Error('Please enter a name');
+        } else if(!validator.isAlpha(name)) {
+            throw new Error('Name must be a string');
         }
-        res.status(201).json({message: 'New user created', data :newUser});
+        const newUser = await User.create(req.body);
+        return res.status(201).json({message: 'New user created', data: newUser});
     } catch (err) {
+        console.log(err.message);
         res.status(400).json({error: err.message});
     }
+    next()
 }
