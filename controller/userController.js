120564debf517d15c25121d3bda00287e2179db5
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-12 09:56:32 +0100

    addition of delete user route

--- controller/userController.js
+++ controller/userController.js
@@ -49,7 +49,21 @@ exports.modify = async(req, res, next)=> {
         if(!updateInfo) {
             return  res.status(404).json({error:`No such user with the ID ${id}`})
         }
-        res.status(200).json({message: `User with ID ${id} updated successful`, data: updateInfo});
+        res.status(200).json({message: `User with ID ${id} updated successfully`, data: updateInfo});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+    next();
+}
+
+exports.remove = async(req, res, next)=> {
+    const id = req.params.id;
+    try {
+        const deleteUser = await User.findByIdAndDelete(id);
+        if(!deleteUser) {
+            return  res.status(404).json({error:`No such user with the ID ${id}`})
+        }
+        res.status(200).json({message: `User with ID ${id} deleted`});
     } catch (err) {
         res.status(500).json({error: err.message});
     }
--- router/userRoute.js
+++ router/userRoute.js
@@ -6,5 +6,6 @@ Router.post('/', userController.create);
 Router.get('/', userController.all);
 Router.get('/:id', userController.single);
 Router.patch('/:id', userController.modify);
+Router.delete('/:id', userController.remove);
 
 module.exports = Router;
