2df66277500702dec6361e15b1eaf14fcbaf12b0
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-13 05:39:08 +0100

    moved database connection to entry file

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const db = async()=> {
     try {
-        await mongoose.connect(process.env.DBURI, {useNewUrlParser: true, useUnifiedTopology: true});
+        await mongoose.connect(process.env.DBURL, {useNewUrlParser: true, useUnifiedTopology: true});
         console.log('Database Connected');
     } catch (err) {
         console.log(err);
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-const db = require('./config/db');
+// const db = require('./config/db');
 const userRouter = require('./router/userRoute');
 const dotenv = require('dotenv');
+const { default: mongoose } = require('mongoose');
 dotenv.config();
-db();
+// db();
 const PORT = process.env.PORT;
+const db = async()=> {
+    try {
+        await mongoose.connect(process.env.DBURL)
+        app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
+    } catch (err) {
+        console.log(err)
+    }
+}
+db()
 // middleware to parse json data
 app.use(express.json());
 // middleware to parse url encoded data
@@ -14,4 +24,4 @@ app.use(express.urlencoded({extended: true}));
 app.use('/api', userRouter);
 
 
-app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
+// app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
