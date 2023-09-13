3d3032f69d950bd4ea0c9ff5345984b5b553f49c
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-13 13:41:07 +0100

    the listen method was used in a promise attached the database function

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const app = express();
-// const db = require('./config/db');
+const db = require('./config/db');
 const userRouter = require('./router/userRoute');
 const dotenv = require('dotenv');
-const { default: mongoose } = require('mongoose');
 dotenv.config();
-// db();
 const PORT = process.env.PORT;
-const db = async()=> {
-    try {
-        await mongoose.connect(process.env.DBURL)
-        app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
-    } catch (err) {
-        console.log(err)
-    }
-}
-db()
 // middleware to parse json data
 app.use(express.json());
 // middleware to parse url encoded data
@@ -23,5 +12,8 @@ app.use(express.urlencoded({extended: true}));
 // user middleware
 app.use('/api', userRouter);
 
-
-// app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
+db()
+.then(()=> {
+    app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
+})
+.catch((err)=> console.log(err))
