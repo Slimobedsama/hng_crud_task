d77299238ac411be254ba70fbdf108aed1ef6013
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-13 02:58:47 +0100

    added the option object to mongoose connect method

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const db = async()=> {
     try {
-        await mongoose.connect(process.env.DBURI);
+        await mongoose.connect(process.env.DBURI, {useNewUrlParser: true, useUnifiedTopology: true});
         console.log('Database Connected');
     } catch (err) {
         console.log(err);
